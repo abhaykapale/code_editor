367bbc46d5d79578e80b34d27fa4e47f56a33f74
abhaykapale <abhayrameshkapale@gmail.com>
2026-05-14 16:36:35 +0530

    langind page added

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import UserButton from "@/modules/auth/components/user-button"; 
-export default function Home() {
-  return (
-    <div >
-      <UserButton></UserButton>
-    </div>
-  );
-}
