f3430d204e676920c110e5641b54f27c2a8c2076
abhaykapale <abhayrameshkapale@gmail.com>
2026-05-21 17:49:32 +0530

    implemented monaco hooks

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -3,10 +3,12 @@ import { Separator } from "@/components/ui/separator";
 import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { TemplateFileTree } from "@/modules/playground/components/playground-explorer";
+import { useFileExplorer } from "@/modules/playground/hooks/useFileExplorer";
 import { usePlayground } from "@/modules/playground/hooks/usePlayground";
+import { TemplateFile } from "@prisma/client";
+import { fi } from "date-fns/locale";
 import { useParams } from "next/navigation";
-import React from "react";
-
+import React, { useEffect } from "react";
 const MainPlaygroundPage =() =>
 {
     const {id} = useParams<{id: string}>();
@@ -20,11 +22,39 @@ const MainPlaygroundPage =() =>
         saveTemplateData,
         isSaving
     } = usePlayground(id);
-    if(isLoading) return <div>Loading...</div>;
-    if(error) return <div>Error: {error}</div>;
-    if(!templateData) return <div>No template found</div>;
 
-    const activeFile= "sample.txt"
+    
+    const {
+        setActiveFileId,
+        playgroundId,
+        setEditorContent,
+        setPlaygroundId,
+        setOpenFiles,
+        editorContent,
+        activeFileId,
+        openFile,
+        openFiles,
+        closeFile,
+        closeAllFiles,
+        setTemplateData,
+
+    }= useFileExplorer();
+
+   useEffect ( ()=>{setPlaygroundId(id)}, [id,setPlaygroundId] );
+   useEffect ( ()=>{
+        if(templateData && !openFiles.length) {
+
+            setTemplateData(templateData);
+
+        }
+    }, [templateData,setTemplateData , openFiles.length] );
+
+    const activeFile= openFiles.find((file)=> file.id===activeFileId)
+    const hasUnsavedChanges = openFiles.some((file)=> file.hasUnsavedChanges)
+    const handleFileSelect = (file : any) {
+        openFile(file)
+    }
+
 
     return (
         <TooltipProvider>
@@ -32,7 +62,7 @@ const MainPlaygroundPage =() =>
 
                 <TemplateFileTree
                     data={templateData}
-                    onFileSelect={()=>{}}
+                    onFileSelect={handleFileSelect}
                     selectedFile={activeFile}
                     title="File Explorer"
                     onAddFile={()=>{}}
